fb9400cc2aaa661be9f9a92ef2843dafd1fca0be
EdaCelikeloglu <edacelikeloglu@gmail.com>
2024-04-24 14:51:33 +0300

    Stream app

---
+++ stream_deneme.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+# Başlık
+st.title('Merhaba, Streamlit!')
+
+# Metin
+st.write('Bu bir Streamlit uygulamasıdır.')
+
+# Grafik
+import matplotlib.pyplot as plt
+import numpy as np
+x = np.linspace(0, 10, 100)
+y = np.sin(x)
+plt.plot(x, y)
+st.pyplot(plt)
