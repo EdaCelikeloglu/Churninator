d825247576ec59652656ae50510d2082b6ee57c6
EdaCelikeloglu <edacelikeloglu@gmail.com>
2024-05-03 21:22:56 +0300

    streamlit 3

--- pages/1_📊_DataFrame_Demo.py
+++ pages/1_📊_DataFrame_Demo.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import altair as alt
 from urllib.error import URLError
+import plotly as px
 import plotly.express as px
 import plotly.graph_objects as go
 from catboost import CatBoostClassifier
--- pages/2_📈_Plotting_Demo.py
+++ pages/2_📈_Plotting_Demo.py
@@ -1,8 +1,13 @@
+import plotly.express as px
 import streamlit as st
 import time
 import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.neighbors import LocalOutlierFactor
 
-st.set_page_config(page_title="Plotting Demo", page_icon="📈")
+st.set_page_config(page_title="Plotting Demo", page_icon="📈", layout="wide")
 
 st.markdown("# Plotting Demo")
 st.sidebar.header("Plotting Demo")
@@ -11,6 +16,188 @@ st.write(
 Streamlit. We're generating a bunch of random numbers in a loop for around
 5 seconds. Enjoy!"""
 )
+
+# Fonksiyonlarımız:
+def grab_col_names(dataframe, cat_th=9, car_th=20):
+    #cat_cols, cat_but_car
+    cat_cols = [col for col in dataframe.columns if dataframe[col].dtypes == "O"]
+    num_but_cat = [col for col in dataframe.columns if dataframe[col].nunique() < cat_th and
+                   dataframe[col].dtypes != "O"]
+    cat_but_car = [col for col in dataframe.columns if dataframe[col].nunique() > car_th and
+                   dataframe[col].dtypes == "O"]
+    cat_cols = cat_cols + num_but_cat
+    cat_cols = [col for col in cat_cols if col not in cat_but_car]
+
+    #num_cols
+    num_cols = [col for col in dataframe.columns if dataframe[col].dtypes != "O"]
+    num_cols = [col for col in num_cols if col not in num_but_cat]
+
+    print(f"Observations: {dataframe.shape[0]}")
+    print(f"Variables: {dataframe.shape[1]}")
+    print(f"cat_cols: {len(cat_cols)}")
+    print(f"num_cols: {len(num_cols)}")
+    print(f"cat_but_car: {len(cat_but_car)}")
+    print(f"num_but_car: {len(num_but_cat)}")
+    return cat_cols, num_cols, cat_but_car
+
+def outlier_thresholds(dataframe, col_name, q1=0.05, q3=0.95):
+    quartile1 = dataframe[col_name].quantile(q1)
+    quartile3 = dataframe[col_name].quantile(q3)
+    interquantile_range = quartile3 - quartile1
+    up_limit = quartile3 + 1.5 * interquantile_range
+    low_limit = quartile1 - 1.5 * interquantile_range
+    return low_limit, up_limit
+
+def check_outlier(dataframe, col_name):
+    low_limit, up_limit = outlier_thresholds(dataframe, col_name)
+    if dataframe[(dataframe[col_name] > up_limit) | (dataframe[col_name] < low_limit)].any(axis=None):
+        return True
+    else:
+        return False
+
+def grab_outliers(dataframe, col_name, index=False):
+    low, up = outlier_thresholds(dataframe, col_name)
+    print(dataframe[((dataframe[col_name] < low) | (dataframe[col_name] > up))].shape[0])
+    # if dataframe[((dataframe[col_name] < low) | (dataframe[col_name] > up))].shape[0] > 10:
+    #     print(dataframe[((dataframe[col_name] < low) | (dataframe[col_name] > up))].head())
+    # else:
+    #     print(dataframe[((dataframe[col_name] < low) | (dataframe[col_name] > up))])
+
+    if index:
+        outlier_index = dataframe[((dataframe[col_name] < low) | (dataframe[col_name] > up))].index
+        return outlier_index
+
+def replace_with_thresholds(dataframe, variable):
+    low_limit, up_limit = outlier_thresholds(dataframe, variable)
+    dataframe.loc[(dataframe[variable] < low_limit), variable] = low_limit
+    dataframe.loc[(dataframe[variable] > up_limit), variable] = up_limit
+
+def cat_summary(dataframe, col_name, plot=False):
+    print(pd.DataFrame({col_name: dataframe[col_name].value_counts(),
+                        "Ratio": 100 * dataframe[col_name].value_counts() / len(dataframe)}))
+    print("##########################################")
+    if plot:
+        sns.countplot(x=dataframe[col_name], data=dataframe)
+        plt.show(block=True)
+
+def num_summary(dataframe, numerical_col, plot=False):
+    quantiles = [0.05, 0.10, 0.20, 0.30, 0.40, 0.50, 0.60, 0.70, 0.80, 0.90, 0.95, 0.99]
+    print(dataframe[numerical_col].describe(quantiles).T)
+
+    if plot:
+        dataframe[numerical_col].hist(bins=20)
+        plt.xlabel(numerical_col)
+        plt.title(numerical_col)
+        plt.show(block=True)
+
+def one_hot_encoder(dataframe, categorical_cols, drop_first=True):
+    dataframe = pd.get_dummies(dataframe, columns=categorical_cols, drop_first=drop_first, dtype=int)
+    return dataframe
+
+def combine_categories(df, cat_col1, cat_col2, new_col_name):
+    df[new_col_name] = df[cat_col1].astype(str) + '_' + df[cat_col2].astype(str)
+
+df = pd.read_csv("BankChurners.csv")
+
+df.drop([
+    "Naive_Bayes_Classifier_Attrition_Flag_Card_Category_Contacts_Count_12_mon_Dependent_count_Education_Level_Months_Inactive_12_mon_1",
+    "Naive_Bayes_Classifier_Attrition_Flag_Card_Category_Contacts_Count_12_mon_Dependent_count_Education_Level_Months_Inactive_12_mon_2"],
+    inplace=True, axis=1)
+
+# Bağımlı değişkenimizin ismini target yapalım ve 1, 0 atayalım:
+df.rename(columns={"Attrition_Flag": "Target"}, inplace=True)
+df["Target"] = df.apply(lambda x: 0 if (x["Target"] == "Existing Customer") else 1, axis=1)
+
+# ID kolonunda duplicate bakıp, sonra bu değişkeni silme
+df["CLIENTNUM"].nunique()  # 10127 - yani duplicate yok id'de
+df.drop("CLIENTNUM", axis=1, inplace=True)
+
+cat_cols, num_cols, cat_but_car = grab_col_names(df)
+
+# Outlier temizleme (IQR ve LOF):
+# IQR
+for col in num_cols:
+    print(col)
+    grab_outliers(df, col)
+
+for col in num_cols:
+    replace_with_thresholds(df, col)
+
+# LOF
+clf = LocalOutlierFactor(n_neighbors=20)
+clf.fit_predict(df[num_cols])
+
+df_scores = clf.negative_outlier_factor_
+
+scores = pd.DataFrame(np.sort(df_scores))
+scores.plot(stacked=True, xlim=[0, 100], style='.-')
+plt.show()
+
+th = np.sort(df_scores)[25]
+
+df[df_scores < th].drop(axis=0, labels=df[df_scores < th].index)
+
+# Missing values
+cols_with_unknown = ['Income_Category', "Education_Level"]
+for col in cols_with_unknown:
+    df[col] = df[col].apply(lambda x: np.nan if x == 'Unknown' else x)
+
+
+# Ürün sayısı arttıkça Churn olasılığı azalıyor
+st.write("Ürün sayısı arttıkça Churn olasılığı azalıyor.")
+mean_target_by_relationship = df.groupby("Total_Relationship_Count")["Target"].mean().reset_index()
+fig = px.bar(mean_target_by_relationship, x="Total_Relationship_Count", y="Target",
+             labels={"Total_Relationship_Count": "Toplam Ürün Sayısı", "Target": "Target"},
+             title="Toplam Ürün Sayısına Göre Target", color_discrete_sequence=["blue"])
+st.plotly_chart(fig)
+
+# Yeni gelen müşteriler risk mi?
+st.write("Yeni gelen müşteriler risk mi?")
+mean_target_by_inactive_months = df.groupby("Months_Inactive_12_mon")["Target"].mean().reset_index()
+fig = px.bar(mean_target_by_inactive_months, x="Months_Inactive_12_mon", y="Target",
+             labels={"Months_Inactive_12_mon": "İnaktif Ay Sayısı", "Target": "Target"},
+             title="İnaktif Ay Sayısına Göre Target", color_discrete_sequence=px.colors.qualitative.Pastel)
+st.plotly_chart(fig)
+
+# Borcu çok olanlar gidemiyor
+mean_utilization_by_target = df.groupby("Target")["Avg_Utilization_Ratio"].mean().reset_index()
+mean_revolving_bal_by_target = df.groupby("Target")["Total_Revolving_Bal"].mean().reset_index()
+fig_utilization = px.bar(mean_utilization_by_target, x="Target", y="Avg_Utilization_Ratio",
+                         labels={"Target": "Hedef", "Avg_Utilization_Ratio": "Borç/Kredi Limiti"},
+                         title="Targete Göre Borç/Kredi Limiti", color_discrete_sequence=px.colors.qualitative.Pastel)
+fig_utilization.update_layout(height=400, width=400)
+fig_revolving_bal = px.bar(mean_revolving_bal_by_target, x="Target", y="Total_Revolving_Bal",
+                           labels={"Target": "Hedef", "Total_Revolving_Bal": "Ortalama Devir Bakiyesi"},
+                           title="Hedefe Göre Ortalama Devir Bakiyesi", color_discrete_sequence=px.colors.qualitative.Pastel)
+fig_revolving_bal.update_layout(height=400, width=400)
+col1, col2 = st.columns(2)
+with col1:
+    st.plotly_chart(fig_utilization)
+with col2:
+    st.plotly_chart(fig_revolving_bal)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 progress_bar = st.sidebar.progress(0)
 status_text = st.sidebar.empty()
